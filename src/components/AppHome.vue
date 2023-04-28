7819f25df9badbbba4113432d27fde4c43b940ff
asads30 <itmasteruz@yandex.com>
2023-04-28 14:28:39 +0500

    02

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -3,7 +3,7 @@
       <div class="header">
         <b-nav pills class="header-nav" slot="header" v-b-scrollspy:nav-scroller>
           <b-nav-item 
-            v-for="category in categories"
+            v-for="category in activeCategories"
             :key="category.id"
             :href="'#cat' + category.id" 
             @click="scrollIntoView"
